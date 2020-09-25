8700855eacb13113819de4ea6cf255a5cbf284bd
1miaocat <67374671+1miaocat@users.noreply.github.com>
2020-09-25 08:14:11 +0700

    Add files via upload

---
+++ placeholder.py
@@ -0,0 +1,7 @@
+verb = input('Enter a verb: ')
+adj =  input('Enter an adjective: ')
+noun1 = input('Enter a noun: ')
+noun2 = input('Enter a noun: ')
+noun3 = input('Ener a noun: ')
+
+print(f'A Shinigami {verb} my {adj} {noun1} out of the {noun2} as if he were a vegetarian fishing a {noun3} out of his salad.')
